3f2570ef1a4136796cd0c6b9776d955250575097
jvr <jvrobert@yahoo.com>
2022-02-19 12:41:32 -0700

    upd

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include "ProvisioningManager.h"
+#include "MqttManager.h"
+#include "Sen0395.h"
+#include "mqtt_util.h"
+
+#define TOPIC "garage_motion"
 
 extern "C" {
     void app_main();
 }
 
+void Motion(bool detected)
+{
+    auto mqtt = MqttManager::GetInstance();
+    std::string msg;
+    AddJsonKv("detected", detected, msg);
+    msg += "}";
+    mqtt->Publish(TOPIC, msg);
+    printf("Motion state: %d\n", detected);
+}
+
 void app_main(void)
 {
-
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    ProvisioningManager::Start(true);
+    MqttManager::Start();
+    Sen0395::Start();
+    Sen0395::GetInstance()->OnDetectionChange(Motion);
 }
